34a3c70188ac49497ecf0a6d1ad7e1e8a2eda99d
ahmad-az1405898 <36534122+ahmad-az1405898@users.noreply.github.com>
2019-02-02 15:28:17 +0300

    nodejs

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+const express = require('express');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+
+const app = express();
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({extended: true});
+
+app.use(jsonParser);
+app.use(urlencodedParser);
+app.use(cors());
+
+
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+if (fs.existsSync('certs/server.crt') && fs.existsSync('certs/server.key')) {
+	const certificate = fs.readFileSync('certs/server.crt').toString();
+	const privateKey = fs.readFileSync('certs/server.key').toString();
+	const options = {key: privateKey, cert: certificate};
+
+	var server = https.createServer(options, app).listen(process.env.PORT || 5000);
+	console.log(`Example app listening on port ${process.env.PORT || 5000} using HTTPS`);
+} else {
+	var server = http.createServer(app).listen(process.env.PORT || 5000);
+	console.log(`Example app listening on port ${process.env.PORT || 5000}`);
+}
